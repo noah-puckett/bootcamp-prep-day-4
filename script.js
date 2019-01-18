421f3f15e38752fbeebea8f69853843029e84b04
Noah Puckett <lanterloo@gmail.com>
2019-01-17 19:23:02 -0800

    mapout greet function

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+//
+
+
+
+var firstName = document.getElementById("first-name")
+var lastName = document.getElementById("last-name")
+
+firstName = firstName.value;
+
 
 
 
 function greet() {
-    console.log('greet function called');   
+    //assign names to variables
+    //get input by id for first and last name
+
+    //get values out of the inputs
+
+    //make greeting string
+    //display greeting
+
+
+    console.log('hello' + firstName + lastName + '!');   
 }
 
